c28090eefebb2fbafdadd6d4c313b064dfabeb9a
Josh Ren <renjiashu84@gmail.com>
2023-08-27 14:06:14 -0700

    mask local path

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from utils import PDf_email_generator
-
-
-File_listener_folder = "C:/Users/renjo/OneDrive/Desktop/Personal/Side_Business/LY/Business/Inovices/Incoming"
-Output_File_Location = "C:/Users/renjo/OneDrive/Desktop/Personal/Side_Business/LY/Business/Inovices"
+import time
+from config import File_listener_folder, Output_File_Location, sleep_time
 
 
 def main():
     PDf_email_generator(File_listener_folder,Output_File_Location).file_runner()
 
 
-if __name__ == "__main__":
-    main()
-
+if __name__ == '__main__':
+    while True:
+        try:
+            main()
+            time.sleep(sleep_time)  # Wait for 500 seconds before the next iteration
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            time.sleep(sleep_time)  # If there's an error, wait for 500 seconds before retrying
 
 
